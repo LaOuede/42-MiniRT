d40e103db463aa7b73b096ef4acc4c75b51a17b1
Gwen <gwenola.leroux@gmail.com>
2023-08-01 14:29:29 -0400

    Vector arithmetic implemented

--- include/miniRT.h
+++ include/miniRT.h
@@ -7,7 +7,34 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-//int	main(void);
-int32_t main(int32_t argc, const char* argv[]);
+/* --------------------SETTINGS-------------------- */
+# define WIDTH 1920
+# define HEIGHT 1080
+
+/* ------------------- STRUCTURE ------------------- */
+
+typedef struct s_vec3
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_vec3;
+
+typedef struct s_rt
+{
+	mlx_t		*mlx;
+	mlx_image_t	*image;
+	keys_t		*keys;
+}	t_rt;
+
+/* ------------------- FUNCTIONS ------------------- */
+t_vec3	vec_add(t_vec3 v1, t_vec3 v2);
+t_vec3	vec_cross(t_vec3 v1, t_vec3 v2);
+float	vec_dot(t_vec3 v1, t_vec3 v2);
+float	vec_mag(t_vec3 v1);
+t_vec3	vec_mult(t_vec3 v1, t_vec3 v2);
+t_vec3	vec_norm(t_vec3 v);
+t_vec3	vec_scale(t_vec3 v, float scale);
+t_vec3	vec_subs(t_vec3 v1, t_vec3 v2);
 
 #endif
