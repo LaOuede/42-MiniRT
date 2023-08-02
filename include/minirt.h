8844043365372f052f8c7631a36e2e6eb26e5557
Gwen <gwenola.leroux@gmail.com>
2023-08-02 15:59:50 -0400

    colors files updated | included in .h

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: gle-roux <gle-roux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 11:06:20 by macote            #+#    #+#             */
-/*   Updated: 2023/08/02 15:05:58 by gle-roux         ###   ########.fr       */
+/*   Updated: 2023/08/02 15:59:36 by gle-roux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -194,6 +194,11 @@ t_mat4	matrix_translation(float tx, float ty, float tz);
 t_vec3	matrix_vec_mult(t_mat4 mat, t_vec3 v);
 
 //colors
-
+t_color	add_2_colors(t_color col1, t_color col2);
+t_color	add_3_colors(t_color col1, t_color col2, t_color col3);
+t_color	color_scale(t_color col1, float scale);
+t_color	sub_2_colors(t_color col1, t_color col2);
+t_color	sub_3_colors(t_color col1, t_color col2, t_color col3);
+int		get_rgba(float r, float g, float b, float a);
 
 #endif
