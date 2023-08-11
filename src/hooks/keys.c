c1b126388901f47ef149d37c827d518bb7ea4787
Gwen <gwenola.leroux@gmail.com>
2023-08-11 08:50:13 -0400

    camera translation keys modified

--- src/hooks/keys.c
+++ src/hooks/keys.c
@@ -56,14 +56,14 @@ void	keys_camera(mlx_key_data_t keydata, void *param)
 	t_minirt	*minirt;
 
 	minirt = (t_minirt *)param;
-	if (mlx_is_key_down(minirt->mlx, MLX_KEY_S)
-		|| mlx_is_key_down(minirt->mlx, MLX_KEY_F))
-		camera_translation_x(minirt, keydata.key);
-	if (mlx_is_key_down(minirt->mlx, MLX_KEY_E)
+	if (mlx_is_key_down(minirt->mlx, MLX_KEY_A)
 		|| mlx_is_key_down(minirt->mlx, MLX_KEY_D))
-		camera_translation_y(minirt, keydata.key);
+		camera_translation_x(minirt, keydata.key);
 	if (mlx_is_key_down(minirt->mlx, MLX_KEY_W)
-		|| mlx_is_key_down(minirt->mlx, MLX_KEY_R))
+		|| mlx_is_key_down(minirt->mlx, MLX_KEY_S))
+		camera_translation_y(minirt, keydata.key);
+	if (mlx_is_key_down(minirt->mlx, MLX_KEY_Q)
+		|| mlx_is_key_down(minirt->mlx, MLX_KEY_E))
 		camera_translation_z(minirt, keydata.key);
 /* 	if (mlx_is_key_down(minirt->mlx, MLX_KEY_L)
 		|| mlx_is_key_down(minirt->mlx, MLX_KEY_J))
@@ -85,8 +85,7 @@ void	keys_exit(void *param)
 	t_minirt	*minirt;
 
 	minirt = (t_minirt *)param;
-	if (mlx_is_key_down(minirt->mlx, MLX_KEY_ESCAPE)
-		|| mlx_is_key_down(minirt->mlx, MLX_KEY_Q))
+	if (mlx_is_key_down(minirt->mlx, MLX_KEY_ESCAPE))
 	{
 		free_minirt();
 		exit(EXIT_SUCCESS);
