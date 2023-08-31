436ddc3ab1ffbc12db3647ee3c225dbe37ea382d
Gwen <gwenola.leroux@gmail.com>
2023-08-31 14:12:23 -0400

    cylinder normal functional hopefully

--- src/intersections/hit_cylinder.c
+++ src/intersections/hit_cylinder.c
@@ -1,13 +1,38 @@
 #include "minirt.h"
 
 /*
+Surface normal vector calcul
+*/
+t_vec3	calc_normal(t_vec3 origin, t_vec3 d, float t, float m, t_cylindre *cyl)
+{
+	// n = nrm(P-C-V*m)
+	t_vec3	P;
+	t_vec3	C;
+	t_vec3	PC;
+	t_vec3	Vm;
+	t_vec3	n;
+
+	P = vec_add(origin, vec_scale(d, t));
+	C = vec_subs(cyl->position, (vec_scale(vec_norm(cyl->direction), cyl->hauteur / 2)));
+	PC = vec_subs(P, C);
+	Vm = vec_scale(vec_norm(cyl->direction), m);
+	n = vec_norm(vec_subs(PC, Vm));
+	return (n);
+}
+
+/*
 
 Equation :
-
-Surface normal vector :
+	a = D|D - (D|V)^2
+	b/2 = D|X - (D|V)*(X|V)
+	c = X|X - (X|V)^2 - r*r
+	d = b² - a * c
+	t = (-b - √d) / a; OUTSIDE
+	t = (-b + √d) / a; INSIDE
+	m = D|V*t + X|V
 
 */
-void	hit_cylinder(t_vec3 d, t_object *packed_cylinder, t_hit *hit, t_vec3 origin)
+void	hit_cylinder_body(t_vec3 d, t_object *packed_cylinder, t_hit *hit, t_vec3 origin)
 {
 	float		a;
 	float		half_b;
@@ -20,14 +45,9 @@ void	hit_cylinder(t_vec3 d, t_object *packed_cylinder, t_hit *hit, t_vec3 origin
 
 	cyl = (t_cylindre *)packed_cylinder->obj;
 	displacement = vec_subs(origin, cyl->position);
-
-	// a = D|D - (D|V)^2
 	a = vec_dot(d, d) - pow(vec_dot(d, vec_norm(cyl->direction)), 2);
-	// b/2 = D|X - (D|V)*(X|V)
 	half_b = vec_dot(d, displacement) - vec_dot(d, vec_norm(cyl->direction)) * vec_dot(displacement, vec_norm(cyl->direction));
-	//c = X|X - (X|V)^2 - r*r
 	c = vec_dot(displacement, displacement) - pow(vec_dot(displacement, vec_norm(cyl->direction)), 2) - pow(cyl->rayon, 2);
-	// d = b² - a * c
 	discriminant = pow(half_b, 2) - (a * c);
 	// No intersection if discriminant < 0
 	if (discriminant < 0.00001)
@@ -36,7 +56,6 @@ void	hit_cylinder(t_vec3 d, t_object *packed_cylinder, t_hit *hit, t_vec3 origin
 		hit->t = ERROR;
 		return ;
 	}
-	// t = (-b - √d) / a;
 	t = (-half_b - sqrtf(discriminant)) / a;
 	m = vec_dot(d, vec_norm(cyl->direction)) * t + vec_dot(displacement, vec_norm(cyl->direction));
 	if (t < 0.00001 || fabs(m) > cyl->hauteur / 2)
@@ -53,43 +72,13 @@ void	hit_cylinder(t_vec3 d, t_object *packed_cylinder, t_hit *hit, t_vec3 origin
 		{
 			hit->obj = packed_cylinder;
 			hit->t = t; // HIT
+			hit->normal_cyl = calc_normal(origin, d, t, m, cyl);
 		}
 	}
 	else
 	{
 		hit->obj = packed_cylinder;
 		hit->t = t; // HIT
+		hit->normal_cyl = calc_normal(origin, d, t, m, cyl);
 	}
-
-	############### Normal test 1 ###################
-	// n = nrm(P-C-V*m)
-/* 	t_vec3	P;
-	t_vec3	C;
-	t_vec3	PC;
-	t_vec3	Vm;
-
-	P = vec_add(origin, vec_scale(d, t));
-	C = vec_subs(cyl->position, (vec_scale(vec_norm(cyl->direction), cyl->hauteur / 2)));
-	PC = vec_subs(P, C);
-	Vm = vec_scale(d, m);
-	hit->normal = vec_subs(PC, Vm);
-	hit->normal = vec_norm(hit->normal); */
-
-
-	############### Normal test 2 ###################
-	t_vec3	hp;
-	t_vec3	C;
-	float T;
-	t_vec3 pt;
-
-	hp = vec_add(origin, vec_scale(d, t));
-	C = vec_subs(cyl->position, (vec_scale(vec_norm(cyl->direction), cyl->hauteur / 2)));
-	T = vec_dot(vec_subs(hp, C), vec_norm(cyl->direction));
-	pt = vec_add(C, vec_scale(vec_norm(cyl->direction), T));
-	hit->normal = vec_norm(vec_subs(hp, pt));
-
-	############### Normal test 3 ###################
-/* 	t_vec3	cross_prod;
-	cross_prod = vec_cross(vec_norm(hit->d), vec_norm(cyl->direction));
-	hit->normal = vec_norm(vec_cross(cross_prod, vec_norm(cyl->direction))); */
 }
