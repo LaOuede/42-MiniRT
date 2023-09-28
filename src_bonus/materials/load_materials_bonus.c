70d1714bf0d4bfc5db2fdec51f1f4b9080beec81
Gwenola Le Roux <gwenola.leroux@gmail.com>
2023-09-28 14:45:07 -0400

    Makefile updated

--- src_bonus/materials/load_materials_bonus.c
+++ src_bonus/materials/load_materials_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   load_materials_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gle-roux <gle-roux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gwenolaleroux <gwenolaleroux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 14:03:29 by gle-roux          #+#    #+#             */
-/*   Updated: 2023/09/19 14:09:16 by gle-roux         ###   ########.fr       */
+/*   Updated: 2023/09/22 17:29:47 by gwenolalero      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
